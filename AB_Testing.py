9106e76f82833d28c71666d31ba075541a766f9f
AZİZE SULTAN PALALI <72896355+azizepalali@users.noreply.github.com>
2025-01-24 23:32:40 +0300

    Update AB_Testing.py

--- AB_Testing.py
+++ AB_Testing.py
@@ -4,7 +4,7 @@ import pandas as pd
 import streamlit as st
 import math
 
-from calculations import calculate_sample_size, ab_test_calculations, ab_test_calculations_overall
+from statistical_calculations import calculate_sample_size, ab_test_calculations, ab_test_calculations_overall
 from utils import default_sql
 
 from plotly.subplots import make_subplots
