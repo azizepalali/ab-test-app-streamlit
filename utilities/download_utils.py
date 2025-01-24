f8d799fddca8b4c8e31aa7d2378824a31013304b
AZİZE SULTAN PALALI <72896355+azizepalali@users.noreply.github.com>
2025-01-25 00:23:48 +0300

    Update download_utils.py

--- utilities/download_utils.py
+++ utilities/download_utils.py
@@ -8,12 +8,12 @@ def get_excel_download_link(df, filename="A_B_Test_Results.xlsx"):
     Parameters:
         df (pd.DataFrame): DataFrame to be downloaded.
         filename (str): Name of the downloaded file.
-    
+
     Returns:
         str: HTML link for downloading the file.
     """
     towrite = io.BytesIO()
-    df.to_excel(towrite, encoding="utf-8", index=False, header=True)
+    df.to_excel(towrite, engine='openpyxl', encoding="utf-8", index=False, header=True)
     towrite.seek(0)
     b64 = base64.b64encode(towrite.read()).decode()
     return f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="{filename}">Download {filename}</a>'
